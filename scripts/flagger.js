41d2b544c729947da36c4003fd9a879757036e5f
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-07-27 17:49:26 +0530

    Flag counter

--- scripts/flagger.js
+++ scripts/flagger.js
@@ -1,6 +1,13 @@
 import { redraw } from "./util";
 
+const flagNumberElement = document.getElementById("flagNumber");
+
+const setCounter = (count) => {
+  flagNumberElement.innerHTML = `${count}`;
+};
+
 export function handleRightClick(grid, mouse, context, n) {
+  let flagCount = parseInt(flagNumberElement.innerHTML);
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j].isCell(mouse.x, mouse.y)) {
@@ -9,8 +16,10 @@ export function handleRightClick(grid, mouse, context, n) {
           grid[i][j].toggleFlagged();
         }
         // if not flagged and not revealed we can flag it
-        else if (!grid[i][j].isFlagged && !grid[i][j].isRevealed)
+        else if (!grid[i][j].isFlagged && !grid[i][j].isRevealed) {
+          setCounter(flagCount - 1);
           grid[i][j].toggleFlagged();
+        }
       }
     }
   }
